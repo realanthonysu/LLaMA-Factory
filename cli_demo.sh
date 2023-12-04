28c08a55928653668ad231dd068a45c47aaa0408
realanthonysu <anthonysu19@gmail.com>
2023-12-04 14:53:11 +0800

    bash cli again

--- cli_demo.sh
+++ cli_demo.sh
@@ -1,5 +1,5 @@
 python src/cli_demo.py \
-    --model_name_or_path THUDM/lora_merge_chatglm3/ \
+    --model_name_or_path THUDM/chatglm3-6b/ \
     --template default \
     --finetuning_type lora \
-    # --checkpoint_dir export_chatglm3/adapter_model.bin 
+    --checkpoint_dir THUDM/export_chatglm3/
--- export_demo.sh
+++ export_demo.sh
@@ -2,5 +2,5 @@ python src/export_model.py \
     --model_name_or_path THUDM/chatglm3-6b/ \
     --template chatglm3 \
     --finetuning_type lora \
-    --checkpoint_dir /export_chatglm3 \
+    --checkpoint_dir THUDM/export_chatglm3 \
     --export_dir THUDM/lora_merge_chatglm3
