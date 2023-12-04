62c1d8155f5ad08a1eaca409d57c71d3b45af6d0
realanthonysu <anthonysu19@gmail.com>
2023-12-04 14:37:46 +0800

    fix cli bash again

--- cli_demo.sh
+++ cli_demo.sh
@@ -2,4 +2,4 @@ python src/cli_demo.py \
     --model_name_or_path THUDM/chatglm3-6b/ \
     --template chatglm3 \
     --finetuning_type lora \
-    --checkpoint_dir /export_chatglm3/adapter_model.bin 
+    --checkpoint_dir export_chatglm3/adapter_model.bin 
