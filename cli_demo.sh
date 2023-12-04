9a4b8f5ce64c55d7d17d11896fc96f341e06d87b
realanthonysu <anthonysu19@gmail.com>
2023-12-04 14:43:15 +0800

    bash update

--- cli_demo.sh
+++ cli_demo.sh
@@ -1,5 +1,5 @@
 python src/cli_demo.py \
     --model_name_or_path THUDM/chatglm3-6b/ \
-    --template chatglm3 \
+    --template default \
     --finetuning_type lora \
     --checkpoint_dir export_chatglm3/adapter_model.bin 
